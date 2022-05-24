2386943d7d9b80602d47c3d5c7f2e50fdc8e957b
Krisna K. pandit <61043995+krisnakumar88@users.noreply.github.com>
2022-05-24 08:26:00 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+import './style.css'
+
+const btnGen  = document.getElementById('btn-pict');
+const selPict = document.getElementById('select-pict');
+const pict    = document.getElementById('img-pict');
+
+const waifuPic = (category = 'waifu') => {
+  
+  fetch(`https://api.waifu.pics/sfw/${category}`)
+  .then(res => res.json())
+  .then(data => pict.src = data.url);
+
+};
+
+btnGen.onclick = () => {
+  pict.value = '';
+  const valSelect = selPict.value;
+
+  if (valSelect == '') {
+    return null;
+  }
+  waifuPic(valSelect)
+
+};
+
+
+
+waifuPic();
